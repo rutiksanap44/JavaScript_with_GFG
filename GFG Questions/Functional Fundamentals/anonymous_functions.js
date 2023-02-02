02e4f99d9d8cad45c99dcec071214b731c57bb1f
rutiksanap44 <99959986+rutiksanap44@users.noreply.github.com>
2023-02-02 13:30:57 +0530

    arrow function started

---
+++ GFG Questions/Functional Fundamentals/anonymous_functions.js
@@ -0,0 +1,8 @@
+// anonymous functions are those who dont have any identity.
+// you cannot call before initialization.
+
+let greetingFunctionvar = function(){
+    console.log("Hola from Rutik");
+}
+
+greetingFunctionvar();
